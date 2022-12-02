6ed5c0c3ce40a72215ba0a27cfac5502dfd830c5
shimizuchisa <112614370+shimizuchisa@users.noreply.github.com>
2022-12-02 08:38:09 +0000

    set root, add conntroller public/home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
-  devise_for :users
+  root to: 'public/homes#top'
+
+  namespace :public do
+    get 'homes/top'
+  end
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # 会員用
+  # URL /users/sign_in ...
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
